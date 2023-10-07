18ea7a14355af534c841a29b48e58234c8b94744
schsdrobotics <schsdrobotics@gmail.com>
2023-10-07 15:18:51 -0400

    refactor: ?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manual/LoopTime.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manual/LoopTime.kt
@@ -3,23 +3,27 @@ package org.firstinspires.ftc.teamcode.manual
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotorEx
+import org.firstinspires.ftc.teamcode.hardware.Drivetrain
 import org.firstinspires.ftc.teamcode.library.LoopTimer
-import kotlin.math.sin
 
 @TeleOp
 class LoopTime : OpMode() {
-    private val motors by lazy { List(4) { hardwareMap["motor$it"] as DcMotorEx } }
+    private val drivetrain by lazy { Drivetrain(this) }
+
     private val timer = LoopTimer()
 
-    override fun init() { }
+    override fun init() { drivetrain }
 
     override fun start() = timer.reset()
 
     override fun loop() {
-        motors.forEach { it.power = 0.5 }
+        val average = timer.average()
 
-        telemetry.addData("last time", timer.times.last())
-        telemetry.addData("average", timer.average())
+        drivetrain.loop()
+
+        telemetry.addData("last time", "${timer.times.last()}ms")
+        telemetry.addData("average", "${average}ms")
+        telemetry.addData("hz", "${1000.0 / average} Hz")
 
         timer.record()
     }
