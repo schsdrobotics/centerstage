43d19d251385458f85ba8478475ca08a0317ab71
schsdrobotics <schsdrobotics@gmail.com>
2023-11-08 16:47:01 -0500

    add: current logging in telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manual/DriverControlled.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manual/DriverControlled.kt
@@ -1,12 +1,18 @@
 package org.firstinspires.ftc.teamcode.manual
 
+import com.qualcomm.hardware.lynx.LynxModule
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit
 import org.firstinspires.ftc.teamcode.hardware.subsystem.DriveSubsystem
 import org.firstinspires.ftc.teamcode.hardware.subsystem.IntakeSubsystem
 import org.firstinspires.ftc.teamcode.hardware.subsystem.LiftSubsystem
-import org.firstinspires.ftc.teamcode.hardware.subsystem.LiftSubsystem.Position.*
+import org.firstinspires.ftc.teamcode.hardware.subsystem.LiftSubsystem.Position.HIGH
+import org.firstinspires.ftc.teamcode.hardware.subsystem.LiftSubsystem.Position.LOW
+import org.firstinspires.ftc.teamcode.hardware.subsystem.LiftSubsystem.Position.MID
+import org.firstinspires.ftc.teamcode.hardware.subsystem.LiftSubsystem.Position.ZERO
 import org.firstinspires.ftc.teamcode.hardware.subsystem.WristSubsystem
 import org.mercurialftc.mercurialftc.scheduler.OpModeEX
+
 
 @TeleOp(group = "!")
 class DriverControlled : OpModeEX() {
@@ -43,7 +49,15 @@ class DriverControlled : OpModeEX() {
     override fun init_loopEX() {}
     override fun startEX() {}
     override fun loopEX() {
+        var current = 0.0
 
+        val allHubs = hardwareMap.getAll(LynxModule::class.java)
+
+        for (hub in allHubs) {
+            current += hub.getCurrent(CurrentUnit.AMPS)
+        }
+
+        telemetry.addData("current", current)
     }
     override fun stopEX() {}
 }
