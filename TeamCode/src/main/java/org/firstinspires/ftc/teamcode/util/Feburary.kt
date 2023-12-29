1e7ca281173b7f7c02fd378b08ac3e0f9263ec49
robopandas <schsdrobotics@gmail.com>
2023-12-29 18:18:22 -0500

    refactor: `Feburary` as described below
    - specify units
      - `DistanceUnit.INCH`
      - `AngleUnit.RADIANS`
    
    - `feed()` `Mode`s:
      - `Average`: utilizes the average the values of all of the `detections`
      - `Target(Int)`: only uses positional data from the targeted tag, does nothing if that tag is not detected
    
      - `CAMERA_OFFSET` to account for the camera not being in the center
        - TODO: tune this offset and check if subtraction is the correct approach

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Feburary.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Feburary.kt
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
+import org.firstinspires.ftc.teamcode.util.extensions.*
 import org.firstinspires.ftc.teamcode.util.structures.None
 import org.firstinspires.ftc.teamcode.util.structures.Option
 import org.firstinspires.ftc.teamcode.util.structures.Some
@@ -34,10 +35,8 @@ class Feburary(val hardwareMap: HardwareMap, val opmode: OpMode) {
         }
 
     val averagePosition: Option<AprilTagPoseFtc>
-        get() = if (detected) {
-            val average: AprilTagPoseFtc = detections.map { it.ftcPose }.map {  }
-
-            Some(average)
+        get() = if (detections.isNotEmpty()) {
+            Some(detections.map { it.ftcPose }.reduce { a, b -> a + b } / detections.size)
         } else {
             None()
         }
@@ -50,31 +49,35 @@ class Feburary(val hardwareMap: HardwareMap, val opmode: OpMode) {
         opmode.telemetry.addData("detected", currentDetections.map { it.id }.joinToString(", "))
     }
 
-    fun feed() = when (detected) {
-        is None -> Vector3.Zero
-        is Some -> {
-            val tag = (detected as Some<AprilTagDetection>).value
+    fun feed(mode: Mode = Average): Vector3 {
+        return when (detected) {
+            is None -> Vector3.Zero
+            is Some -> {
+                val tag = (detected as Some).value
 
-            if (tag.id != DESIRED.value) Vector3.Zero
+                if (mode is Target && tag.id != mode.target) Vector3.Zero
 
-            val (bearing, range, yaw) = Triple(tag.ftcPose.bearing, tag.ftcPose.range, tag.ftcPose.yaw)
+                val position = if (mode is Target) tag.ftcPose else (averagePosition as Some).value
 
-            val (x, y, z) = Triple(sin(bearing) * range, cos(bearing) * range, -yaw)
+                val (bearing, range, yaw) = Triple(position.bearing, position.range, position.yaw)
+                val (x, y, z) = Triple(sin(bearing) * range, cos(bearing) * range, -yaw)
 
-            opmode.telemetry.addData("x", x * Gains.X)
-            opmode.telemetry.addData("y", (y - DESIRED_PROXIMITY) * Gains.Y)
-            opmode.telemetry.addData("z", z * Gains.Z)
+                opmode.telemetry.addData("x", (x - CAMERA_OFFSET) * Gains.X)
+                opmode.telemetry.addData("y", (y - DESIRED_PROXIMITY) * Gains.Y)
+                opmode.telemetry.addData("z", z * Gains.Z)
 
-            opmode.telemetry.addData("pose x", tag.ftcPose.x)
-            opmode.telemetry.addData("pose y", tag.ftcPose.y)
-            opmode.telemetry.addData("pose z", tag.ftcPose.z)
+                opmode.telemetry.addData("pose x", position.x)
+                opmode.telemetry.addData("pose y", position.y)
+                opmode.telemetry.addData("pose z", position.z)
 
-            Vector3(x * Gains.X, y * Gains.Y, z * Gains.Z)
+                Vector3(x * Gains.X, y * Gains.Y, z * Gains.Z)
+            }
         }
     }
 
     companion object {
         private const val DESIRED_PROXIMITY = 3.0
+        private const val CAMERA_OFFSET = 6.0
 
         private object Gains {
             const val X = -0.05
@@ -88,7 +91,10 @@ class Feburary(val hardwareMap: HardwareMap, val opmode: OpMode) {
             Right
         }
 
-        private val DESIRED = Some(5)
+        sealed class Mode
+        object Average : Mode()
+        class Target(val target: Int) : Mode()
+
 
         private val DESIRE_MAP = mapOf(
             Position.Left   to listOf(1, 4),
